fa14fdbba6099e12bb6af51a0f4cf36afe70b017
seohyeon <uimi3606@gmail.com>
2024-01-03 23:11:58 +0900

    init

---
+++ src/main/java/com/example/ogett/OGetTApplication.java
@@ -0,0 +1,13 @@
+package com.example.ogett;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OGetTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OGetTApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ogett/OGetTApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ogett;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OGetTApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
