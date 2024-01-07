13e8c4d8ab824feafc16a81c16d993cc6d4927f8
seohyeon <uimi3606@gmail.com>
2024-01-08 00:08:56 +0900

    FEAT Controller 추가

--- src/main/java/com/example/ogett/Controller/MemberController.java
+++ src/main/java/com/example/ogett/Controller/MemberController.java
@@ -6,14 +6,46 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MemberController {
 
-    @GetMapping("/login")
-    public String login(){
-        return "member/login";
-    }
-
     @GetMapping("/")
     public String home(){
         return "index";
     }
+
+    @GetMapping("/login")
+    public String login(){
+        return "member/Login";
+    }
+
+    @GetMapping("/cart")
+    public String cart(){
+        return "Cart";
+    }
+
+    @GetMapping("/ai")
+    public String ai(){
+        return "AI";
+    }
+
+    @GetMapping("/membership")
+    public String membership(){
+        return "Membership";
+    }
+
+    @GetMapping("/payment")
+    public String payment(){
+        return "Payment";
+    }
+
+    @GetMapping("/product")
+    public String product(){
+        return "Product";
+    }
+
+    @GetMapping("/wishlist")
+    public String wishlist(){
+        return "Wishlist";
+    }
+
+
 
 }
