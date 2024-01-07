c40a2542f171cca861d26a08ec6ca460edb8de5e
박찬윤 <qkcksdbs@naver.com>
2024-01-08 01:35:34 +0900

    맴버 생성과 맴버서비스 기초

--- src/main/java/com/example/ogett/Entity/Member.java
+++ src/main/java/com/example/ogett/Entity/Member.java
@@ -1,8 +1,29 @@
 package com.example.ogett.Entity;
 
-import jakarta.persistence.Entity;
+import com.example.ogett.Constant.Gender;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-public class Member {
+@Entity
+@Table(name = "member")
+@Getter @Setter
+@ToString
+public class Member{
 
+    @Id
+    @Column(name = "member_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
+    @Column(unique = true)
+    private String email;
+    private String name;
+    private String phoneNum;
+    private String birthdate;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "gender")
+    private Gender gender; // Gender 열거형 타입
 }
