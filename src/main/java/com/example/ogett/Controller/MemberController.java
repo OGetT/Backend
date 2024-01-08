0d78845a83ac0e6453745b1978f36021bcac11c7
eunbin <eunbin2158@gmail.com>
2024-01-08 19:56:45 +0900

    htmlupdate

--- src/main/java/com/example/ogett/Controller/MemberController.java
+++ src/main/java/com/example/ogett/Controller/MemberController.java
@@ -3,7 +3,7 @@ package com.example.ogett.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class MemberController {
 
     @RequestMapping("/")
@@ -14,7 +14,7 @@ public class MemberController {
 
     @RequestMapping("/login")
     public String login(){
-        return "member/Login.html";
+        return "member/Login";
     }
 
     @RequestMapping("/cart")
