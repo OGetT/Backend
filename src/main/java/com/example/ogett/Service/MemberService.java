d0fde531d4b97f7c79e1a9f100383f08c5552018
eunbin <eunbin2158@gmail.com>
2024-01-08 22:04:46 +0900

    리다이렉트 부분 수정중

--- src/main/java/com/example/ogett/Service/MemberService.java
+++ src/main/java/com/example/ogett/Service/MemberService.java
@@ -3,14 +3,19 @@ package com.example.ogett.Service;
 import com.example.ogett.DTO.MemberDTO;
 import com.example.ogett.Entity.Member;
 import com.example.ogett.Repository.MemberRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
 
+    private final MemberRepository memberRepository;
+
     @Autowired
-    private MemberRepository memberRepository;
+    private MemberService(MemberRepository memberRepository){this.memberRepository=memberRepository;}
 
     public void registerMember(MemberDTO memberDTO) {
         Member member = new Member();
@@ -23,5 +28,16 @@ public class MemberService {
 
         memberRepository.save(member);
     }
+//    @Transactional
+//    public boolean join(Member member) {
+//        // 사용자 등록 로직을 추가
+//        // 예를 들어, 이미 등록된 아이디가 아니라면 등록하고 true 반환, 이미 등록된 아이디라면 false 반환
+//        if (memberRepository.findById(member.getId()).isEmpty()) {
+//            memberRepository.save(member);
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
 }
 
--- src/test/java/com/example/ogett/Service/MemberServiceTest.java
+++ src/test/java/com/example/ogett/Service/MemberServiceTest.java
@@ -4,7 +4,7 @@ import com.example.ogett.Constant.Gender;
 import com.example.ogett.DTO.MemberDTO;
 import com.example.ogett.Entity.Member;
 import com.example.ogett.Repository.MemberRepository;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
