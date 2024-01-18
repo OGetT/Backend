5d50835d2dc5d63dab776e30d729ba95ada2b1cd
박찬윤 <qkcksdbs@naver.com>
2024-01-18 13:48:00 +0900

    html add

---
+++ src/main/resources/templates/mainTitle.jsx
@@ -0,0 +1,19 @@
+import { Link, useNavigate } from "react-router-dom";
+import React from "react";
+
+const MainTitle = () => {
+  const navigate = useNavigate();
+  return (
+    <p className="flex justify-center pb-12 w-full pt-12" href="index.html" onClick={() => {
+      navigate("/");
+    }}>
+      <Link to="/">
+        <span style={{ fontFamily: 'dattebayo' }} className="text-[80px] font-bold text-green-900 m-0 text-TGreen">
+          GreenShop
+        </span>
+      </Link>
+    </p>
+  );
+}
+
+export default MainTitle;
