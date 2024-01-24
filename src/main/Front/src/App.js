3449ac17a117921cd7e55b8667e9c826163f334f
seohyeon <uimi3606@gmail.com>
2024-01-24 22:21:38 +0900

    FIX 회원가입 리액트 페이지 연결

--- src/main/Front/src/App.js
+++ src/main/Front/src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import axios from "axios";
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
 
 export default function App() {
+
+    useEffect(() => {
+        axios.get('/api/hello')
+            .then(response => setHello(response.data))
+            .catch(error => console.log(error))
+    }, []);
+
+    return (
+        <div>
+            백엔드에서 가져온 데이터입니다 : {hello}
+        </div>
+    );
+
+
   return (
     <Router>
       <Routes>
