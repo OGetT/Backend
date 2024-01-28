a2567d13c418dcf4ba05b492a337ee06c9bd6985
seohyeon <uimi3606@gmail.com>
2024-01-29 00:33:17 +0900

    ADD cart 브랜치 생성

--- src/main/java/com/example/ogett/Controller/CartController.java
+++ src/main/java/com/example/ogett/Controller/CartController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CartController {
 
-    @GetMapping("/Cart")
+    @GetMapping("/cart")
     public String ProductRegistration() {
         return "/Cart/Cart";
     }
